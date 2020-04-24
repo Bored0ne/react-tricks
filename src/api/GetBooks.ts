03031834c7f2906310136bdbac265c1e01be0dba
Stephen Seaton-Blanchard <stephen@seatoncode.com>
2020-04-24 12:20:14 -0500

    Adds reattempting logic

--- src/api/GetBooks.ts
+++ src/api/GetBooks.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {Book} from "../models/Book";
 import PureCache from 'pure-cache';
+import Reattempt from 'reattempt';
 
 // Found this on https://github.com/public-apis/public-apis
 const baseUrl: string = 'https://openlibrary.org/search.json';
@@ -12,11 +13,14 @@ export async function GetBooks(title): Promise<Book[]> {
     let result = cacheStore.get(title)?.value;
     console.log(result);
     if (!result) {
-        let bookResult = await axios.get(baseUrl, {
-            params: {
-                q: title || ""
-            }
-        });
+        let bookResult = await Reattempt.run(
+            {times: 3},
+            async () => await axios.get(baseUrl, {
+                params: {
+                    q: title || ""
+                }
+            })
+        );
         result = bookResult?.data?.docs;
         cacheStore.put(title, result, FIVE_MINUTES)
     }
