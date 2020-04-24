924d7713b12426aec0129ad2a36c0cf50cf6b3c4
Stephen Seaton-Blanchard <stephen@seatoncode.com>
2020-04-24 12:13:07 -0500

    Adds Caching to get request

--- src/api/GetBooks.ts
+++ src/api/GetBooks.ts
@@ -1,15 +1,24 @@
 import axios from 'axios';
 import {Book} from "../models/Book";
+import PureCache from 'pure-cache';
 
 // Found this on https://github.com/public-apis/public-apis
-const baseUrl: string = 'http://openlibrary.org/search.json';
+const baseUrl: string = 'https://openlibrary.org/search.json';
+const cacheStore = new PureCache({expiryCheckInterval: 500});
+const FIVE_MINUTES = 5 * 60 * 1000;
 
 // @ts-ignore
 export async function GetBooks(title): Promise<Book[]> {
-    let bookResult = await axios.get(baseUrl, {
-        params: {
-            q: title || ""
-        }
-    });
-    return bookResult?.data?.docs;
+    let result = cacheStore.get(title)?.value;
+    console.log(result);
+    if (!result) {
+        let bookResult = await axios.get(baseUrl, {
+            params: {
+                q: title || ""
+            }
+        });
+        result = bookResult?.data?.docs;
+        cacheStore.put(title, result, FIVE_MINUTES)
+    }
+    return result;
 }
--- src/util/queueCall.js
+++ src/util/queueCall.js
@@ -1,7 +1,7 @@
 // This is a trick to allow someone to keep typing in an input and
 // have that input cause an effect {delay} number of millis after they are done typing.
 const queue = [];
-export function queueCall(key, callback, delay = 3000) {
+export function queueCall(key, callback, delay = 2000) {
     if (queue[key]) {
         clearTimeout(queue[key]);
     }
